b4d9d782bd0b6f9ba9617cd7120ecdd1fcefcad6
mwjames <jamesin.hongkong.1@gmail.com>
2016-06-22 06:13:40 +0200

    1.0.0 release preparations

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,12 +4,16 @@ if ( PHP_SAPI !== 'cli' ) {
 	die( 'Not an entry point' );
 }
 
+error_reporting( E_ALL | E_STRICT );
+date_default_timezone_set( 'UTC' );
+ini_set( 'display_errors', 1 );
+
 if ( !is_readable( $autoloaderClassPath = __DIR__ . '/../../SemanticMediaWiki/tests/autoloader.php' ) ) {
-	die( 'The SemanticMediaWiki test autoloader is not available' );
+	die( 'The Semantic MediaWiki test autoloader is not available' );
 }
 
 if ( ( $version = SemanticScribunto::getVersion() ) === null ) {
-	die( "\SemanticScribunto is not loaded, please check your LocalSettings or Composer settings.\n" );
+	die( "\Semantic Scribunto is not available, please check your Composer or LocalSettings.\n" );
 }
 
 print sprintf( "\n%-20s%s\n", "Semantic Scribunto: ", $version );
