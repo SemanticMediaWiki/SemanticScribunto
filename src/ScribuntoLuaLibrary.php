8cfb93182db9a8cf7aa6754145a8d96ce54495ba
matthew-a-thompson <mathompson@mitre.org>
2016-09-09 11:54:00 -0400

    Update ScribuntoLuaLibrary.php
    Simplify return statement in getQueryResult

--- src/ScribuntoLuaLibrary.php
+++ src/ScribuntoLuaLibrary.php
@@ -57,7 +57,7 @@ class ScribuntoLuaLibrary extends Scribunto_LuaLibraryBase {
 		$api->execute();
 		$data = $api->getResult()->getResultData();
 
-		return $data === null ? array( null ) : array( $data );
+		return array( $data );
 	}
 
 	/**
