8dc98aca18820ce35b88ee4de54ef008598c95f6
Tobias Oetterer <oetterer@uni-paderborn.de>
2017-01-04 14:31:07 +0100

    added integration tests, mostly for mw.smw.set; fixed some issues with SemanticScribuntoJsonTestCaseScriptRunnerTest

--- tests/phpunit/Integration/JSONScript/Fixtures/module.smw.lua
+++ tests/phpunit/Integration/JSONScript/Fixtures/module.smw.lua
@@ -31,6 +31,42 @@ function p.getQueryResult( frame )
     return convertResultTableToString( queryResult.results )
 end
 
+function p.set( frame )
+    if not mw.smw then
+        return "mw.smw module not found"
+    end
+
+    local result = mw.smw.set( frame.args )
+    if result == true then
+        return 'Your data was stored successfully'
+    else
+        return 'An error occurred during the storage process. Message reads ' .. result.error
+    end
+end
+
+function p.setWithSeparator( frame )
+    if not mw.smw then
+        return "mw.smw module not found"
+    end
+
+    if not frame.args.sep or not frame.args.property or not frame.args.data then
+        return p.set( frame )
+    end
+
+    local query = {
+        frame.args.property .. '=' .. frame.args.data,
+        '+sep=' .. frame.args.sep
+    }
+
+    local result = mw.smw.set( query )
+
+    if result == true then
+        return 'Your data was stored successfully'
+    else
+        return 'An error occurred during the storage process. Message reads ' .. result.error
+    end
+end
+
 function convertResultTableToString( queryResult )
 
     local queryResult = queryResult
@@ -55,6 +91,25 @@ function convertResultTableToString( queryResult )
     end
 
     return queryResult
+end
+
+function varDump( entity, indent )
+    local entity = entity
+    local indent = indent and indent or ''
+    if type( entity ) == 'table' then
+        local output = '(table)[' .. #entity .. ']:'
+        indent = indent .. '  '
+        for k, v in pairs( entity ) do
+            output = output .. '\n' .. indent .. k .. ': ' .. varDump( v, indent )
+        end
+        return output
+    elseif type( entity ) == 'function' then
+        return '(function)'
+    elseif type( entity ) == 'bool' then
+        return '(bool)' .. ( entity and 'TRUE' or 'FALSE' )
+    else
+        return '(' .. type( entity ) .. ') ' .. entity
+    end
 end
 
 return p
