9c90a7be6b44b539651981213a1a2670b7526498
mwjames <jamesin.hongkong.1@gmail.com>
2019-08-04 16:09:51 +0000

    Use `LightweightJsonTestCaseScriptRunner`, refs 30 (#67)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,20 +8,26 @@ error_reporting( E_ALL | E_STRICT );
 date_default_timezone_set( 'UTC' );
 ini_set( 'display_errors', 1 );
 
-if ( !is_readable( $autoloaderClassPath = __DIR__ . '/../../SemanticMediaWiki/tests/autoloader.php' ) ) {
+if ( !defined( 'SMW_PHPUNIT_AUTOLOADER_FILE' ) || !is_readable( SMW_PHPUNIT_AUTOLOADER_FILE ) ) {
 	die( "\nThe Semantic MediaWiki test autoloader is not available" );
 }
 
-if ( !class_exists( 'SemanticScribunto' ) || ( $version = SemanticScribunto::getVersion() ) === null ) {
+if ( !defined( 'SMW_SCRIBUNTO_VERSION' ) ) {
 	die( "\nSemantic Scribunto is not available or loaded, please check your Composer or LocalSettings.\n" );
 }
 
-print sprintf( "\n%-20s%s\n", "Semantic Scribunto: ", $version );
-print sprintf( "%-20s%s\n", "Lua version: ", SemanticScribunto::getVersion( 'Lua' ) );
+$lua = 'n/a';
+
+if ( method_exists( 'Scribunto_LuaStandaloneInterpreter', 'getLuaVersion' ) ) {
+	$lua = Scribunto_LuaStandaloneInterpreter::getLuaVersion( [ 'luaPath' => null ] );
+}
+
+print sprintf( "\n%-20s%s\n", "Semantic Scribunto: ", SMW_SCRIBUNTO_VERSION );
+print sprintf( "%-20s%s\n", "Lua version: ", $lua );
 
 // What is the Scribunto version?? Who knows ..., it doesn't have a version number
 
-$autoLoader = require $autoloaderClassPath;
+$autoLoader = require SMW_PHPUNIT_AUTOLOADER_FILE;
 $autoLoader->addPsr4( 'SMW\\Scribunto\\Tests\\', __DIR__ . '/phpunit/Unit' );
 $autoLoader->addPsr4( 'SMW\\Scribunto\\Tests\\Integration\\', __DIR__ . '/phpunit/Integration' );
 unset( $autoLoader );
