5217175623581b0d9a629f39566f29e1ea66ad43
paladox <paladox@users.noreply.github.com>
2025-01-27 16:10:49 +0000

    Update SemanticScribuntoJsonTestCaseScriptRunnerTest.php

--- tests/phpunit/Integration/JSONScript/SemanticScribuntoJsonTestCaseScriptRunnerTest.php
+++ tests/phpunit/Integration/JSONScript/SemanticScribuntoJsonTestCaseScriptRunnerTest.php
@@ -3,19 +3,19 @@
 namespace SMW\Scribunto\Integration\JSONScript;
 
 use SMW\Tests\Integration\JSONScript\JSONScriptTestCaseRunnerTest;
-use SMW\Tests\JsonTestCaseScriptRunner;
+use SMW\Tests\JSONScriptTestCaseRunner;
 
 /**
  * @see https://github.com/SemanticMediaWiki/SemanticMediaWiki/tree/master/tests#write-integration-tests-using-json-script
  *
- * `JsonTestCaseScriptRunner` provisioned by SMW is a base class allowing to use a JSON
+ * `JSONScriptTestCaseRunner` provisioned by SMW is a base class allowing to use a JSON
  * format to create test definitions with the objective to compose "real" content
  * and test integration with MediaWiki, Semantic MediaWiki, and Scribunto.
  *
  * The focus is on describing test definitions with its content and specify assertions
  * to control the expected base line.
  *
- * `JsonTestCaseScriptRunner` will handle the tearDown process and ensures that no test
+ * `JSONScriptTestCaseRunner` will handle the tearDown process and ensures that no test
  * data are leaked into a production system but requires an active DB connection.
  *
  * @group semantic-scribunto
@@ -30,7 +30,7 @@ use SMW\Tests\JsonTestCaseScriptRunner;
 class SemanticScribuntoJsonTestCaseScriptRunnerTest extends JSONScriptTestCaseRunnerTest {
 
 	/**
-	 * @see JsonTestCaseScriptRunner::getRequiredJsonTestCaseMinVersion
+	 * @see JSONScriptTestCaseRunner::getRequiredJsonTestCaseMinVersion
 	 * @return string
 	 */
 	protected function getRequiredJsonTestCaseMinVersion() {
@@ -38,7 +38,7 @@ class SemanticScribuntoJsonTestCaseScriptRunnerTest extends JSONScriptTestCaseRu
 	}
 
 	/**
-	 * @see JsonTestCaseScriptRunner::getTestCaseLocation
+	 * @see JSONScriptTestCaseRunner::getTestCaseLocation
 	 * @return string
 	 */
 	protected function getTestCaseLocation() {
@@ -46,7 +46,7 @@ class SemanticScribuntoJsonTestCaseScriptRunnerTest extends JSONScriptTestCaseRu
 	}
 
 	/**
-	 * @see JsonTestCaseScriptRunner::getPermittedSettings
+	 * @see JSONScriptTestCaseRunner::getPermittedSettings
 	 */
 	protected function getPermittedSettings() {
 		$settings = parent::getPermittedSettings();
