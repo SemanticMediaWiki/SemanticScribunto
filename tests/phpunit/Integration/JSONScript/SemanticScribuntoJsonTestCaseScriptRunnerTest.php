bc74477344ed463deaa06c6ca49997c53d99d959
paladox <paladox@users.noreply.github.com>
2025-02-21 13:38:38 +0000

    Update SemanticScribuntoJsonTestCaseScriptRunnerTest.php

--- tests/phpunit/Integration/JSONScript/SemanticScribuntoJsonTestCaseScriptRunnerTest.php
+++ tests/phpunit/Integration/JSONScript/SemanticScribuntoJsonTestCaseScriptRunnerTest.php
@@ -2,51 +2,35 @@
 
 namespace SMW\Scribunto\Integration\JSONScript;
 
-use SMW\DIWikiPage;
-use SMW\Scribunto\HookRegistry;
-use SMW\Tests\JsonTestCaseFileHandler;
-use SMW\Tests\JsonTestCaseScriptRunner;
-use SMW\Tests\LightweightJsonTestCaseScriptRunner;
+use SMW\Tests\Integration\JSONScript\JSONScriptTestCaseRunnerTest;
+use SMW\Tests\JSONScriptTestCaseRunner;
 
 /**
  * @see https://github.com/SemanticMediaWiki/SemanticMediaWiki/tree/master/tests#write-integration-tests-using-json-script
  *
- * `JsonTestCaseScriptRunner` provisioned by SMW is a base class allowing to use a JSON
+ * `JSONScriptTestCaseRunner` provisioned by SMW is a base class allowing to use a JSON
  * format to create test definitions with the objective to compose "real" content
  * and test integration with MediaWiki, Semantic MediaWiki, and Scribunto.
  *
  * The focus is on describing test definitions with its content and specify assertions
  * to control the expected base line.
  *
- * `JsonTestCaseScriptRunner` will handle the tearDown process and ensures that no test
+ * `JSONScriptTestCaseRunner` will handle the tearDown process and ensures that no test
  * data are leaked into a production system but requires an active DB connection.
  *
  * @group semantic-scribunto
+ * @group Database
  * @group medium
  *
- * @license GNU GPL v2+
+ * @license GPL-2.0-or-later
  * @since 1.0
  *
  * @author mwjames
  */
-class SemanticScribuntoJsonTestCaseScriptRunnerTest extends LightweightJsonTestCaseScriptRunner {
+class SemanticScribuntoJsonTestCaseScriptRunnerTest extends JSONScriptTestCaseRunnerTest {
 
 	/**
-	 * @var HookRegistry
-	 */
-	private $hookRegistry;
-
-	protected function setUp() {
-		parent::setUp();
-
-		$this->hookRegistry = new HookRegistry();
-
-		$this->hookRegistry->clear();
-		$this->hookRegistry->register();
-	}
-
-	/**
-	 * @see JsonTestCaseScriptRunner::getRequiredJsonTestCaseMinVersion
+	 * @see JSONScriptTestCaseRunner::getRequiredJsonTestCaseMinVersion
 	 * @return string
 	 */
 	protected function getRequiredJsonTestCaseMinVersion() {
@@ -54,7 +38,7 @@ class SemanticScribuntoJsonTestCaseScriptRunnerTest extends LightweightJsonTestC
 	}
 
 	/**
-	 * @see JsonTestCaseScriptRunner::getTestCaseLocation
+	 * @see JSONScriptTestCaseRunner::getTestCaseLocation
 	 * @return string
 	 */
 	protected function getTestCaseLocation() {
@@ -62,19 +46,19 @@ class SemanticScribuntoJsonTestCaseScriptRunnerTest extends LightweightJsonTestC
 	}
 
 	/**
-	 * @see JsonTestCaseScriptRunner::getPermittedSettings
+	 * @see JSONScriptTestCaseRunner::getPermittedSettings
 	 */
 	protected function getPermittedSettings() {
-		parent::getPermittedSettings();
+		$settings = parent::getPermittedSettings();
 
-		return [
+		return array_merge( $settings, [
 			'smwgNamespacesWithSemanticLinks',
 			'smwgPageSpecialProperties',
 			'smwgMaxNonExpNumber',
 			'wgLanguageCode',
 			'wgContLang',
 			'wgLang'
-		];
+		] );
 	}
 
 }
