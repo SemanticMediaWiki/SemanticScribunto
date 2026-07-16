4f97efb7882be1f01ad5d7107bc9e9bd20fa749e
paladox <paladox@users.noreply.github.com>
2026-07-15 23:54:21 -0400

    Update ScribuntoLuaEngineTestBase.php

--- tests/phpunit/Unit/ScribuntoLuaEngineTestBase.php
+++ tests/phpunit/Unit/ScribuntoLuaEngineTestBase.php
@@ -53,7 +53,7 @@ abstract class ScribuntoLuaEngineTestBase extends LuaEngineTestBase {
 	/**
 	 * Only needed for MW 1.31
 	 */
-	public function run( TestResult $result = null ): TestResult {
+	public function run( ?TestResult $result = null ): TestResult {
 		// MW 1.31
 		$this->setCliArg( 'use-normal-tables', true );
 
